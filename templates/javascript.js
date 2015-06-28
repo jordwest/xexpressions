8778f4b63710cef52962fde427079361a41b682f
Jordan West <jordwest@gmail.com>
2015-06-28 16:47:07 +0900

    Wrap xexpression js in closure

--- templates/javascript.js
+++ templates/javascript.js
@@ -2,40 +2,45 @@
 // https://github.com/jordwest/xexpressions
 // Template: {{.TemplateFilename}}
 
-var XExpression = function(regexp) {
-  this.regexp = regexp;
-  this.groups = {'full': 0 };
-}
+window.XEXPRESSIONS = (function(){
 
-XExpression.prototype.exec = function(text) {
-  var result = this.regexp.exec(text);
-  if(result === null || result.length < 1) {
-    return null;
+  var XExpression = function(regexp) {
+    this.regexp = regexp;
+    this.groups = {'full': 0 };
   }
-  var match_data = {};
-  for(group_name in this.groups) {
-    match_data[group_name] = result[this.groups[group_name]];
+
+  XExpression.prototype.exec = function(text) {
+    var result = this.regexp.exec(text);
+    if(result === null || result.length < 1) {
+      return null;
+    }
+    var match_data = {};
+    for(group_name in this.groups) {
+      match_data[group_name] = result[this.groups[group_name]];
+    }
+    return match_data;
   }
-  return match_data;
-}
-XExpression.prototype.test = function(text) {
-  return this.regexp.test(text);
-}
-XExpression.prototype.add_capture_group = function(index, name) {
-  this.groups[name] = index;
-  return this;
-}
+  XExpression.prototype.test = function(text) {
+    return this.regexp.test(text);
+  }
+  XExpression.prototype.add_capture_group = function(index, name) {
+    this.groups[name] = index;
+    return this;
+  }
 
-var xExpressions = {};
+  var xExpressions = {};
 
-{{range .RegularExpressions}}
-/**
-  * {{.TextName}}
-  * {{Line .TextName}}
-  * {{.Description}}
-  * Definition: {{.Source}}
-  */
-xExpressions.{{LowerCase .TextName}} = new XExpression(/{{.RegexpText}}/);
-xExpressions.{{LowerCase .TextName}}{{range .CaptureGroups}}
-  .add_capture_group({{.Index}}, '{{LowerCase .Name}}'){{end}};
-{{end}}
+  {{range .RegularExpressions}}
+  /**
+    * {{.TextName}}
+    * {{Line .TextName}}
+    * {{.Description}}
+    * Definition: {{.Source}}
+    */
+  xExpressions.{{UpperCase .TextName}} = new XExpression(/{{.RegexpText}}/);
+  xExpressions.{{UpperCase .TextName}}{{range .CaptureGroups}}
+    .add_capture_group({{.Index}}, '{{LowerCase .Name}}'){{end}};
+  {{end}}
+
+  return xExpressions;
+})();
